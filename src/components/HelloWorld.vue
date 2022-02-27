5e15ea91a4c8f4a7d6036bf0a3d28a2560166e1b
CengChao <chao1.zeng@aihuishou.com>
2022-02-27 12:54:42 +0800

    feat: 删除lock文件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,7 +27,6 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-    <div>为啥不展示呢</div>
   </div>
 </template>
 
